b98058258a11298fb78b694f3ff63871771fcc13
Krivorot <>
2023-04-28 20:59:48 +0300

    Добавил DataBase_main.cpp с докблоками

---
+++ Программа/ConsoleApplication1/ConsoleApplication1/DataBase_main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @brief Класс для работы с базой данных
+ * @author Криворот Даниил Романович
+ */
+class DB {
+	string password;
+	string login;
+	string ip;
+	string port;
+
+	DB() {
+		// Constructor
+	}
+
+	~DB() {
+		// Destructor
+	}
+
+	/**
+	 * @brief Добавляет заметку в базу данных
+	 * @param note Заметка, которая будет включена в базу данных
+	 * @author Криворот Даниил Романович
+	 */
+	void addNote(const Note& note) {
+		// Добавление заметки 
+	}
+
+	deleteNote() {
+		// Удаление заметки
+	}
+
+	modifyNote() {
+		// Изменение заметки
+	}
+}
